e6cd13b11993e3a8c144eafccfcd2984cb56e20c
Davi <daviist156@gmail.com>
2022-04-24 11:19:10 -0300

    Ifandelse

---
+++ EstudoJava/ifelse.java
@@ -0,0 +1,26 @@
+package EstudoJava;
+
+import java.util.Scanner;
+
+public class ifelse {
+
+public static void main (String[] args){
+
+    Scanner scan = new Scanner(System.in);
+    int hora;
+
+    System.out.println("Por favor, nos informe as horas: ");
+    hora = sc.netxInt();
+     
+    if (hora < 12) {
+        System.out.println("Bom dia" \n);
+    }
+    else {
+    System.out.println("Boa tarde" \n);
+    }
+    
+    sc.close();
+
+    }
+
+}
